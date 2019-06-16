6b5e705cb95bd2dc49c8c17dac1bf46324b247d4
Kajal Goel <kajal11122000@gmail.com>
2019-06-16 13:06:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,22 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+
+  function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+      }
+    } 
+    else {
+      index = 0;
+    }
+  } 
 }
